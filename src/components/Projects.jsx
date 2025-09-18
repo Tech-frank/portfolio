641a756ae7a7f61a36108b777c86c3ec5d3a4091
franklyn Etu <franklynetu80@gmail.com>
2025-09-18 12:57:33 +0100

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import portfolio from '../assets/portfolio.png'
 import lancelot from '../assets/lancelot.png'
 import profis from '../assets/profis.png'
+import novoi from '../assets/novoi.png'
 export default function Projects(){
     return (
         <>
@@ -11,7 +12,7 @@ export default function Projects(){
                     <div className="bg-white rounded-lg shadow-md overflow-hidden">
                         <img src={portfolio} alt="Project 1" className="w-full h-48 object-cover"/>
                         <div className="p-4">
-                            <h2 className="text-xl font-semibold mb-2">Project One</h2>
+                            <h2 className="text-xl font-semibold mb-2">My Porfolio</h2>
                             <p className="text-gray-600 mb-4">This the Portfolio Website I Built For Myself.</p>
                             <a href="#" className="text-blue-500 hover:underline">View Project</a>
                         </div>
@@ -20,7 +21,7 @@ export default function Projects(){
                     <div className="bg-white rounded-lg shadow-md overflow-hidden">
                         <img src={lancelot} alt="Project 2" className="w-full h-48 object-cover"/>
                         <div className="p-4">
-                            <h2 className="text-xl font-semibold mb-2">Project Two</h2>
+                            <h2 className="text-xl font-semibold mb-2">Lancelot</h2>
                             <p className="text-gray-600 mb-4">A saas Application for managing tasks and projects.</p>
                             <a href="https://my-expo-app-six.vercel.app/" className="text-blue-500 hover:underline">View Project</a>
                         </div>
@@ -29,9 +30,18 @@ export default function Projects(){
                     <div className="bg-white rounded-lg shadow-md overflow-hidden">
                         <img src={profis} alt="Project 3" className="w-full h-48 object-cover"/>
                         <div className="p-4">
-                            <h2 className="text-xl font-semibold mb-2">Project Three</h2>
+                            <h2 className="text-xl font-semibold mb-2">PROFIS</h2>
                             <p className="text-gray-600 mb-4">A tool for the project team of a company - Atlantic Fluids and Integrated Services Limited - to help them manage their workflows efficiently.</p>
                             <a href="https://profis-fleet-forge.vercel.app/" className="text-blue-500 hover:underline">View Project</a>
+                        </div>
+                    </div>
+
+                     <div className="bg-white rounded-lg shadow-md overflow-hidden">
+                        <img src={novoi} alt="Project 4" className="w-full h-48 object-cover"/>
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold mb-2">Novoi Auto Detailing</h2>
+                            <p className="text-gray-600 mb-4">A website i built for Novoi, an Auto Detailing business to help showcase their business and allow customers book their services</p>
+                            <a href="https://novoi-site.vercel.app/" className="text-blue-500 hover:underline">View Project</a>
                         </div>
                     </div>
                 </div>
